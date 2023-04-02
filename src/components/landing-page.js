dccf7821f4b41e37c401332eadef19d244732046
rafeq abu fiad <rafeqabufiad@rafeqs-MacBook-Pro.local>
2023-04-02 15:47:29 +0300

    update project

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,36 +1,17 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../style/landing-style.css'
+import Users from '../userData.json'
 
-let userArray = [
-  {
-    name: "Mona",
-    color: "blue"
-  },
-  {
-    name: "Jasmyne",
-    color: "red"
-  },
-  {
-    name: "Aura",
-    color: "green"
-  },
-  {
-    name: "Tina",
-    color: "yellow"
-  },
-]
+let userArray =Users.users
 
 
 function Landing() {
   let user=useState(userArray)
   return (
   <div >
-    
     <h1>HOW'S WATCHING?</h1>
-   
-    <div className='container' >
-      {user[0].map((user)=><div key={user.name} className='square'  style={{background:user.color}}>{user.name}</div>)}
-    </div>
+      <Link to="/Catalog" className='container'>{user[0].map((user)=><div key={user.name} className='square'  style={{background:user.color}}>{user.name}</div>)}</Link>
   </div>
   );
 }
